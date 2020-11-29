8bead6040edd7086ec241f902bb00c730ce243db
AymeneL0 <72787631+AymeneL0@users.noreply.github.com>
2020-11-29 13:24:32 +0100

    Update projet_info_f - C.py

--- projet_info_f - C.py
+++ projet_info_f - C.py
@@ -1,5 +1,4 @@
 ## Projet informatique
-
 ## importation
 import numpy as np
 from numpy import arange
@@ -9,14 +8,15 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import math as m
 from math import *
-## Importation et lecture de fichier csv
-dn=pd.read_excel (r'C:\Users\MOI\Desktop\cours EIVP\cours S1\Algorithmique Programmation\projet info\EIVP_donnée_.xlsx')
-print(dn)
-x=dn.values
-#### Afficher des courbes montrant l’évolution d’une variable en fonction du temps.
 
+## Importation et lecture de fichier csv
+dn=pd.read_excel (r'C:\Users\MOI\Desktop\cours EIVP\cours S1\Algorithmique Programmation\projet info\EIVP_donnée_.xlsx') ## on ouvre le ficher excel contenant toutes les données
+x=dn.values ## chaque colonne du tableau est transformée en une liste. x est une liste de listes
+
+#### Afficher des courbes montrant l’évolution d’une variable en fonction du temps.
 ## Etape 1: convertir les colonnes en liste et les séparer
-id=[]
+
+id=[] ## on cherche à avoir une liste pour chaque colonne
 noise=[]
 temp=[]
 humidity=[]
@@ -33,7 +33,7 @@ for i in range(len(x)):
     time.append(x[i][6])
 
 ## Etape 2: séparer les listes de données en focntion de l'id
-time1=[]
+time1=[] ## Les données sont relevées par plusieurs capteurs. Donc on sépare chaque liste en 6 listes chacunes correspondant à un identifiant (on réalise ce programme pour toutes les données)
 time2=[]
 time3=[]
 time4=[]
@@ -52,7 +52,6 @@ for i in range(len(time)):
         time5.append(time[i])
     else:
         time6.append(time[i])
-
 ##
 noise1=[]
 noise2=[]
@@ -73,10 +72,9 @@ for i in range(len(time)):
         noise5.append(noise[i])
     else:
         noise6.append(noise[i])
-
 ##
 temp1=[]
-temp2=[]
+temp2=[
 temp3=[]
 temp4=[]
 temp5=[]
@@ -92,9 +90,8 @@ for i in range(len(time)):
         temp4.append(temp[i])
     elif id[i]==5:
         temp5.append(temp[i])
-    else:
+   else:
         temp6.append(temp[i])
-
 ##
 humidity1=[]
 humidity2=[]
@@ -115,7 +112,6 @@ for i in range(len(time)):
         humidity5.append(humidity[i])
     else:
         humidity6.append(humidity[i])
-
 ##
 co2_1=[]
 co2_2=[]
@@ -136,10 +132,29 @@ for i in range(len(time)):
         co2_5.append(co2[i])
     else:
         co2_6.append(co2[i])
-
-
 ##
-time1str=[]
+lum1=[]
+lum2=[]
+lum3=[]
+lum4=[]
+lum5=[]
+lum6=[]
+for i in range(len(time)):
+    if id[i]==1:
+        lum1.append(lum[i])
+    elif id[i]==2:
+        lum2.append(lum[i])
+    elif id[i]==3:
+        lum3.append(lum[i])
+    elif id[i]==4:
+        lum4.append(lum[i])
+    elif id[i]==5:
+        lum5.append(lum[i])
+    else:
+        lum6.append(lum[i])
+  
+##
+time1str=[] ## afin de pouvoir traiter le format dates et l'afficher sur les courbes on a transformé en format str.
 for i in range(len(time1)):
     time1str.append(datetime.strptime(time1[i],'%Y-%m-%d %H:%M:%S %z'))
 ##
@@ -162,9 +177,10 @@ for i in range(len(time5)):
 time6str=[]
 for i in range(len(time6)):
     time6str.append(datetime.strptime(time6[i],'%Y-%m-%d %H:%M:%S %z'))
+    
 ## tracer les grahiques
 ##
-def tracer_graph(l,L):
+def tracer_graph(l,L): ## Pour le trcé de toutes les courbes il faut que les listes soient relatives aux mêmes identifiants et que utiliser les liste temps en format str
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plt.plot(l,L)
@@ -174,7 +190,8 @@ def tracer_graph(l,L):
     plt.title("Evolution d'une variabe en fonction du temps selon l'identifiant du capteur")
     plt.show()
 ##
-print(tracer_graph(time4str,co2_4))
+print(tracer_graph(time4str,co2_4)) ## test
+    
 ## Tracer graphique en affichant des valeurs statistiques
 def tracer_graph_max(l,L):
     fig = plt.figure()
@@ -190,12 +207,14 @@ def tracer_graph_max(l,L):
     xmax = l[xpos]
 
     ax.annotate('max', xy=(xmax, ymax), xytext=(xmax, ymax+1),
-               arrowprops=dict(facecolor='green', shrink=0.01),
-               )
 
+               arrowprops=dict(facecolor='green', shrink=0.01),
+
+               )
     plt.show()
 ##
-print(tracer_graph_max(time4str,noise4))
+print(tracer_graph_max(time4str,noise4)) ## test
+    
 ##
 def tracer_graph_min(l,L):
     fig = plt.figure()
@@ -211,14 +230,17 @@ def tracer_graph_min(l,L):
     xmin = l[xpos]
 
     ax.annotate('min', xy=(xmin, ymin), xytext=(xmin, ymin+1),
-               arrowprops=dict(facecolor='red', shrink=0.01),
-               )
 
+               arrowprops=dict(facecolor='red', shrink=0.01),
+
+               
     plt.show()
+
 ##
-print(tracer_graph_min(time5str,humidity5))
+print(tracer_graph_min(time5str,humidity5)) ## test
+
 ##
-def tracer_graph_moyenne(l,L):
+def tracer_graph_moyenne(l,L): ## Cette moyenne concerne toutes les données à part la donnée acoustique, pour laquelle il ne serait pas logique de faire une moyenne qui n'est pas logarithmique.
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plt.plot(l,L)
@@ -229,10 +251,36 @@ def tracer_graph_moyenne(l,L):
 
     y_mean = [np.mean(L)]*len(l)
     mean_line = ax.plot(l,y_mean, label='MOYENNE', linestyle='--')
+                
     legend = ax.legend(loc='upper right')
+    ax.text(min(l),max(L),np.mean(L),bbox=dict(boxstyle = "square",facecolor = "yellow"))
     plt.show()
 ##
-print(tracer_graph_moyenne(time2str,temp2))
+print(tracer_graph_moyenne(time2str,temp2)) ## test
+
+##
+
+def tracer_graph_moyenne_acoustique(l,L): ## Cette moyenne concerne la donnée acoustique on applique la formule de la moyenne énérgetique
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(l,L)
+    plt.xlabel("temps")
+    plt.ylabel("variable")
+    plt.xticks(rotation=90)
+    plt.title("Evolution du bruit en fonction du temps et les données statistiques selon l'identifiant du capteur")
+
+    liste1=[]
+    for i in range (len(L)):
+        int=L[i]/10
+        liste1.append(10**int)
+
+    acoustqique_mean=[10*(m.log10(np.mean(liste1)))]*len(l)
+                
+    ax.text(min(l),max(L),np.mean(L),bbox=dict(boxstyle = "square",facecolor = "yellow"))
+    plt.show()
+##
+print(tracer_graph_moyenne_acoustique(time2str,lum2))
+
 ##
 def tracer_graph_variance(l,L):
     fig = plt.figure()
@@ -244,10 +292,12 @@ def tracer_graph_variance(l,L):
     plt.title("Evolution dune variable en fonction du temps et les données statistiques selon l'identifiant du capteur")
 
     variance=np.var(L)
+
     ax.text(min(l),max(L), variance,bbox=dict(boxstyle = "square",facecolor = "yellow"))
     plt.show()
 ##
-print(tracer_graph_variance(time6str,noise6))
+print(tracer_graph_variance(time6str,noise6)) ## test
+
 ##
 def tracer_graph_ecarttype(l,L):
     fig = plt.figure()
@@ -260,39 +310,26 @@ def tracer_graph_ecarttype(l,L):
 
     variance=np.var(L)
     ecarttype=sqrt(variance)
+
     ax.text(min(l),max(L), ecarttype,bbox=dict(boxstyle = "square",facecolor = "yellow"))
     plt.show()
 ##
-print(tracer_graph_ecarttype(time6str,noise6))
+print(tracer_graph_ecarttype(time6str,noise6)) ## test
+
 ##
-def calcul_median(liste):
-    if len(liste)%2!=0:
-        med1=(len(liste)-1)/2
-        return liste[med1]
-    elif len(liste)%2==0:
-        med2=len(liste)/2
-        med3=(len(iste)/2)-1
-        return np.mean(liste[med2],liste[med3])
-
-def tracer_graph_medianne(l,L):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    plt.plot(l,L)
-    plt.xlabel('temps')
-    plt.ylabel('variable')
-    plt.xticks(rotation=90)
-    plt.title("Evolution dune variable en fonction du temps et les données statistiques selon l'identifiant du capteur")
-
-    median_line = ax.plot(l[calcul_median(L)], label='MEDIANE', linestyle='--')
-    legend = ax.legend(loc='upper right')
-
-    plt.show()
+def calcul_median(liste): ## on distibgue la cas où la liste a une longueur paire et le cas où la longueur est impaire
+    n=len(liste)
+    if n%2==1:
+        return liste[int(n/2)]
+    else:
+        return ((liste[(n//2)-1]+liste[n//2])/2)
 ##
-print(tracer_graph_medianne(time5str,noise5))
+print(calcul_median(humidity2)) ## test
+
 
 ## Calculer la température de rosée et en déduire l'humidex
 ##id1
-    a=17.27
+    a=17.27 ## on utilise la formule de l'humidex disponible sur internet, sauf que pour calculer l'humidex il faut calculer d'abord la temperature de rosée. On reproduit la même fopnction 6 fois (pour chaque capteur).
     b=237.7
     t_ros1=[]
     for X in temp1:
@@ -318,6 +355,7 @@ print(tracer_graph_medianne(time5str,noise5))
         for Y in t_ros2:
             c2=(0.555*(6.11*m.exp((1/273.16)-(1/Y))-10))
         humidex2.append(X+c2)
+
 ##id3
     t_ros3=[]
     for X in temp3:
@@ -330,6 +368,7 @@ print(tracer_graph_medianne(time5str,noise5))
         for Y in t_ros3:
             c2=(0.555*(6.11*m.exp((1/273.16)-(1/Y))-10))
         humidex3.append(X+c2)
+
 ##id4
     t_ros4=[]
     for X in temp4:
@@ -342,6 +381,7 @@ print(tracer_graph_medianne(time5str,noise5))
         for Y in t_ros4:
             c2=(0.555*(6.11*m.exp((1/273.16)-(1/Y))-10))
         humidex4.append(X+c2)
+
 ##id5
     t_ros5=[]
     for X in temp5:
@@ -354,6 +394,7 @@ print(tracer_graph_medianne(time5str,noise5))
         for Y in t_ros5:
             c2=(0.555*(6.11*m.exp((1/273.16)-(1/Y))-10))
         humidex5.append(X+c2)
+
 ##id6
     t_ros6=[]
     for X in temp6:
@@ -366,6 +407,7 @@ print(tracer_graph_medianne(time5str,noise5))
         for Y in t_ros6:
             c2=(0.555*(6.11*m.exp((1/273.16)-(1/Y))-10))
         humidex6.append(X+c2)
+
 ##
 def tracer_humidex(l,L):
     fig = plt.figure()
@@ -377,19 +419,57 @@ def tracer_humidex(l,L):
     plt.title("Evolution de l'humidex en fonction de la température selon l'identifiant' ")
     plt.show()
 ##
-print(tracer_humidex(temp6,humidex6))
+print(tracer_humidex(temp6,humidex6)) ## test
+
 ## Calculer l’indice de corrélation entre un couple de variables
 def calculer_correlation(liste1,liste2):
     cov=np.cov(liste1, liste2, ddof=0)[0][1]
     cor=cov/(sqrt((np.var(liste1))*(np.var(liste2))))
     return cor
 ##
-print(calculer_correlation(temp1,noise1))
+print(calculer_correlation(temp1,noise1)) ## test
+                
+##
+def tracer_graph_anomalie(l,L,p):
+    anomalies_L=[]
+    anomalies_l=[]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(l,L)
+    plt.xlabel("temps")
+    plt.ylabel(L)
+    plt.xticks(rotation=90)
+    plt.title("Evolution d'une variabe en fonction du temps selon l'identifiant du capteur")
 
-##on décompose les dates
-date=[]
-heure=[]
-n=len(time)
-for i in range(n):
-    date.append(time[i][:10])
-    heure.append(time[11:19])
+    for i in range (1,len(l)-1):
+        min=((L[i-1]+L[i+1])/2)-((L[i-1]+L[i+1])/2)*(p/100)
+        max=((L[i-1]+L[i+1])/2)+((L[i-1]+L[i+1])/2)*(p/100)
+        if min>L[i] or max<L[i]:
+            anomalies_L.append(L[i])
+            anomalies_l.append(l[i])
+
+    for i in range (0,len(anomalies_l)):
+        plt.scatter(x=anomalies_l[i], y=anomalies_L[i], color='red')
+
+
+    plt.show()
+
+##
+def horaires():
+    ouverture=[]#on crée une liste des horaires des ouvertures et des fermetures que chaque méthodes nous renvoit
+    fermeture=[]
+    for i in range(len(time1)):
+        if noise1[i-1]<28 and noise1[i+1]>28:#on repere que le bruit se stabilise à 27 la nuit, on utilise la meme logique pour les autres paramètres
+            ouverture.append(time1[i])
+        if lum1[i-1]<5 and lum[i+1]>5:
+            ouverture.append(time1[i])
+        if co2_1[i-1]<425 and co2_1[i+1]>425:
+            ouverture.append(time1[i])
+        if noise1[i-1]>28 and noise1[i+1]<28:
+            fermeture.append(time1[i])
+        if lum1[i-1]>5 and lum[i+1]<5:
+            fermeture.append(time1[i])
+        if co2_1[i-1]>425 and co2_1[i+1]<425:
+            fermeture.append(time1[i])
+    return "les horaires d'ouverture estimés sont", np.median(np.array(ouverture)), np.median(np.array(fermeture))#on retourne la médiane des valeurs deja obtenues
+
